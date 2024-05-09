edc9d8f7c5cfcbf883a1bf3b18d628c432248d35
Ying Zhan <153290203+yingzhan11@users.noreply.github.com>
2024-05-09 11:59:46 +0300

    Update ft_bzero.c

--- ft_bzero.c
+++ ft_bzero.c
@@ -16,26 +16,3 @@ void	ft_bzero(void *s, size_t n)
 {
 	ft_memset(s, 0, n);
 }
-/*
-#include <strings.h>
-#include <stdio.h>
-int	main(void)
-{
-	char	str1[43];
-	char	str2[43];
-	size_t	n;
-
-	n = 42;
-	str1[42] = 'A';
-	str2[42] = 'A';
-	//printf("str1: %s\n", str1);
-	//printf("str2: %s\n", str2);
-	//after bzero;
-	bzero(str1, n);
-	printf("bzero: %s\n", str1);
-	ft_bzero(str2, n);
-	printf("ft: %s\n", str2);
-    printf("%lu, %lu\n", strlen(str1), strlen(str2));
-	return (0);
-}
-*/

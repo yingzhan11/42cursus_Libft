527dfaaaf90613cbfad4f805b2903caa00e83c0d
Ying Zhan <153290203+yingzhan11@users.noreply.github.com>
2024-11-05 10:52:32 +0200

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -26,3 +26,22 @@ void	*ft_memcpy(void *dst, const void *src, size_t n)
 	}
 	return (dst);
 }
+/*
+#include <string.h>
+#include <stdio.h>
+int	main(void)
+{
+	char	*dst1 = "";
+	char	*dst2 = "";
+	char	*src = "22";
+	size_t	n;
+	n = 2;
+	printf("dst1 is %s\ndst2 is %s\nsrc is %s\n", dst1, dst2, src);
+	printf("copy %zu chars from src to dst\n", n);
+	memcpy(NULL, src, n);
+	printf("after memcpy:\ndst1 is %s\nsrc is %s\n", dst1, src);
+	ft_memcpy(NULL, src, n);
+	printf("after ft_memcpy: \ndst2 is %s\nsrc is %s\n", dst2, src);
+	return (0);
+}
+*/

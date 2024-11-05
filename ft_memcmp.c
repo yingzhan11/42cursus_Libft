124e1fa2cd29f94cb6ac283e7f56255dea837572
Ying Zhan <153290203+yingzhan11@users.noreply.github.com>
2024-11-05 10:51:37 +0200

    Update ft_memcmp.c

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -27,3 +27,17 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (0);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	char	s1[10] = "1234\200";
+	char	s2[10] = "1234\0";
+	int		n = 5;
+	printf("s1: %s\ns2: %s\nn: %d\n", s1, s2, n);
+	printf("memcmp: %d\n", memcmp(s1, s2, n));
+	printf("ft_memcmp: %d\n", ft_memcmp(s1, s2, n));
+	return (0);
+}
+*/

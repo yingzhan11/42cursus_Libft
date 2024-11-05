ad6515253b601a873eb57b7d96b168781aab9dec
Ying Zhan <153290203+yingzhan11@users.noreply.github.com>
2024-11-05 10:51:12 +0200

    Update ft_memchr.c

--- ft_memchr.c
+++ ft_memchr.c
@@ -25,3 +25,21 @@ void	*ft_memchr(const void *s, int c, size_t n)
 	}
 	return (NULL);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	char	str[] = "abcdefgh";
+	int		c = 'D';
+	size_t	n = 4;
+	char	*m1;
+	char	*m2;
+	printf("str: %s\n", str);
+	m1 = memchr(str, c, n);
+	printf("memchr: %s\n", m1);
+	m2 = ft_memchr(str, c, n);
+	printf("ft_memchr: %s\n", m2);
+	return (0);
+}
+*/

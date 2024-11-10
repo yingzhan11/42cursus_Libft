db85eb24c6b67f04f035f6fb5c75f420bf2deeda
Ying Zhan <153290203+yingzhan11@users.noreply.github.com>
2024-11-10 14:55:54 +0200

    Update ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -31,3 +31,25 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	dst[i + j] = '\0';
 	return (i + srcsize);
 }
+/*
+#include <stdio.h>
+#include <string.h>
+int	main(void)
+{
+	char	src[20] = "12345";
+	char	dst1[20] = "00";
+	char	dst2[20] = "00";
+	size_t	dstsize = 5;
+	size_t	d_strlcat;
+	size_t	d_ft;
+	printf("src: %s, dstsize: %zu\n", src, dstsize);
+	printf("before strlcat, dst1: %s\n", dst1);
+	d_strlcat = strlcat(dst1, src, dstsize);
+	printf("dst1: %s, dst+src length: %zu\n", dst1, d_strlcat);
+	
+	printf("before ft_strlcat, dst2: %s\n", dst2);
+	d_ft = ft_strlcat(dst2, src, dstsize);
+	printf("dst2: %s, dst+src length: %zu\n", dst2, d_ft);
+	return(0);
+}
+*/
